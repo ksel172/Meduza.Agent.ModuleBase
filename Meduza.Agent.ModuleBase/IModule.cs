3318b7b54bf8c59e834e2d05afaf45abdff672f9
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2025-01-26 01:08:46 +0200

    Initial commit

---
+++ Meduza.Agent.ModuleBase/IModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Meduza.Agent.ModuleBase
+{
+    public interface IModule
+    {
+        string Name { get; }
+
+        string Author { get; }
+
+        string Description { get; }
+
+        string FileName { get; }
+
+        string Usage { get; }
+
+        List<ICommand> Commands { get; set; }
+    }
+}
